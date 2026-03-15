dcaec655f4c4a0cb90603774dd9c2c4b9edc861e
yk <1876421041@qq.com>
2026-03-15 16:16:31 +0800

    Simplify sysupgrade: self-compiled firmware is not signed

---
+++ qsdk/target/linux/feeds/ipq53xx/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,26 @@
+# 自行编译的固件没签名，得跳过绝大多数检查
+
+PART_NAME=firmware
+REQUIRE_IMAGE_METADATA=1
+
+RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
+platform_check_image() {
+	return 0;
+}
+
+platform_do_upgrade() {
+	board=$(board_name)
+	case $board in
+	h3c,ne36-pro)
+		nand_do_upgrade "$1"
+		;;
+	jdcloud,be6500|\
+	jdcloud,er2)
+		kernelname="0:HLOS"
+		rootfsname="rootfs"
+		mmc_do_upgrade "$1"
+		;;
+	esac
+}
